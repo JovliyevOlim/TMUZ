e52b0b7ad4ced09a87be5022b4f2a37bb1d66b42
JovliyevOlim <olimjovliyev98@gmail.com>
2024-10-23 21:59:12 +0500

    efef

--- src/pages/Device/DeviceQrCode.tsx
+++ src/pages/Device/DeviceQrCode.tsx
@@ -4,7 +4,8 @@ import QRCode from 'react-qr-code';
 import { localUrl } from '../../helpers/api_helpers.ts';
 import { Dialog, DialogBackdrop, DialogPanel } from '@headlessui/react';
 import { useReactToPrint } from 'react-to-print';
-
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 export const DeviceQrCode = ({ modalOpen, setModalOpen, item, setItem }: any) => {
   const dispatch: any = useDispatch();
@@ -23,7 +24,33 @@ export const DeviceQrCode = ({ modalOpen, setModalOpen, item, setItem }: any) =>
       setItem(null);
     }
   }, [dispatch, isAction]);
-  console.log(localUrl + '/deviceInfo/' + item?.id);
+
+
+  const downloadPdf = () => {
+    const divElement = document.getElementById('content');
+    html2canvas(divElement).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const pdf = new jsPDF('p', 'mm', 'a4');
+      const imgWidth = 210; // A4 format bo'yicha kenglik (mm)
+      const pageHeight = 295; // A4 format bo'yicha balandlik (mm)
+      const imgHeight = (canvas.height * imgWidth) / canvas.width;
+      let heightLeft = imgHeight;
+      let position = 0;
+
+      pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+      heightLeft -= pageHeight;
+
+      while (heightLeft >= 0) {
+        position = heightLeft - imgHeight;
+        pdf.addPage();
+        pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight);
+        heightLeft -= pageHeight;
+      }
+
+      pdf.save(`${item?.name}.pdf`);
+    });
+  };
+
   return (
     <Dialog open={modalOpen} onClose={tog_standard} className="relative z-9999">
       <DialogBackdrop
@@ -38,8 +65,8 @@ export const DeviceQrCode = ({ modalOpen, setModalOpen, item, setItem }: any) =>
             transition
             className="relative transform w-full overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
           >
-            <div className={'p-8'}  ref={contentRef}>
-              <div style={{ height: 'auto', margin: '0 auto', maxWidth: 300, width: '100%' }}>
+            <div className={'p-8'} ref={contentRef}>
+              <div id="content" style={{ height: 'auto', margin: '0 auto', maxWidth: 300, width: '100%' }}>
                 <QRCode
                   size={256}
                   style={{ height: 'auto', maxWidth: '100%', width: '100%' }}
@@ -57,11 +84,11 @@ export const DeviceQrCode = ({ modalOpen, setModalOpen, item, setItem }: any) =>
                 Cancel
               </button>
               <button
-                onClick={reactToPrintFn}
+                onClick={downloadPdf}
                 type="submit"
                 className="flex justify-center rounded-md bg-blue-700 px-3 py-2 text-sm font-semibold text-white shadow-sm ring-1 ring-inset ring-blue-800 hover:bg-blue-600 sm:mt-0 sm:w-auto"
               >
-                Print
+                Download PDF
               </button>
             </div>
           </DialogPanel>
